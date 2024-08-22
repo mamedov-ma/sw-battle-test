0808e61d5983699cde21df6ba85064d7efd1279e
Mamedov Mail <71010140+mamedov-ma@users.noreply.github.com>
2024-08-22 09:11:55 +0300

    add optional to allow zero id

--- src/sw/map.hpp
+++ src/sw/map.hpp
@@ -4,6 +4,7 @@
 #include "../IO/System/details/PrintFieldVisitor.hpp"
 #include "../IO/System/EventLog.hpp"
 
+#include <optional>
 #include <cstdint>
 #include <vector>
 
@@ -27,21 +28,21 @@ public:
         this->width = width;
         this->height = height;
         cells.resize(width);
-        for (auto& row : cells) { row.resize(height, 0); }
+        for (auto& row : cells) { row.resize(height, std::nullopt); }
         log(tick, io::MapCreated{width, height});
     }
 
-    uint32_t getCell(pos_t pos) const { return cells[pos.x][pos.y]; }
-    bool isCellOccupied(pos_t pos) const { return cells[pos.x][pos.y] != 0; }
+    uint32_t getCell(pos_t pos) const { return *cells[pos.x][pos.y]; }
+    bool isCellOccupied(pos_t pos) const { return cells[pos.x][pos.y].has_value(); }
 
     void occupyCell(pos_t pos, uint32_t id) { cells[pos.x][pos.y] = id; }
-    void freeCell(pos_t pos) { cells[pos.x][pos.y] = 0; }
+    void freeCell(pos_t pos) { cells[pos.x][pos.y].reset(); }
 
     uint32_t getWidth() const { return width; }
     uint32_t getHeight() const { return height; }
 
 private:
-    std::vector<std::vector<uint32_t>> cells;
+    std::vector<std::vector<std::optional<uint32_t>>> cells;
     uint32_t width;
     uint32_t height;
 };
